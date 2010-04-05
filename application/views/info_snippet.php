ad44497898338e4d0c655fa318c5c417cf40a855
claus <claus@0a101332-4a5c-4009-ba00-5ad6272eac1f>
2010-04-05 13:20:13 +0000

    Causes a division by zero on unparsed files.
    git-svn-id: svn://celaneo.local/GPSViz/tags/ver_1.0.0@724 0a101332-4a5c-4009-ba00-5ad6272eac1f

--- application/views/info_snippet.php
+++ application/views/info_snippet.php
@@ -29,7 +29,9 @@
         <tr><th>Started:</th><td><?php echo date('r', $gps->date); ?></td></tr>
         <tr><th>Distance:</th><td><?php echo number_format($gps->distance / 1000, 2); ?> km</td></tr>
         <tr><th>Speed:</th><td><?php echo number_format($gps->speed * 3.6, 2); ?> km/h</td></tr>
+        <?php if ($gps->speed != 0): ?>
         <tr><th>Pace:</th><td><?php echo number_format(60 / ($gps->speed * 3.6), 0); ?> min/km</td></tr>
+        <?php endif; ?>
         <tr><th>Top Speed:</th><td><?php echo number_format($gps->top_speed * 3.6, 2); ?> km/h</td></tr>
         <tr><th>Time Taken:</th><td><?php echo date_diff($gps->total_time_taken); ?></td></tr>
         <tr><th>Coordinates:</th><td><?php echo $gps->coordinates; ?></td></tr>
